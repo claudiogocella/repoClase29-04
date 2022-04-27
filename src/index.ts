913addd200e5898b0fdada11fba405c2185909f4
claudiogocella <104107475+claudiogocella@users.noreply.github.com>
2022-04-27 19:04:08 -0300

    m`sa lineas vamos varias lìneas de còdigo...

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
 console.log("este repo es de la clase del viernes");
+console.log("este repo es de la clase del viernes");
+console.log("este repo es de la clase del viernes");
+console.log("este repo es de la clase del viernes");
+console.log("este repo es de la clase del viernes");
+console.log("este repo es de la clase del viernes");
+console.log("este repo es de la clase del viernes");
