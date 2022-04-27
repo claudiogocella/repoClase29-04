0ba7ff8e843ce4b5a0b18ba2497c162b6c4b5de5
claudiogocella <104107475+claudiogocella@users.noreply.github.com>
2022-04-27 19:10:13 -0300

    agregué una nueva lìnea para probar cómo guardar cambios

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,5 @@ console.log("este repo es de la clase del viernes");
 console.log("este repo es de la clase del viernes");
 console.log("este repo es de la clase del viernes");
 console.log("este repo es de la clase del viernes");
+console.log("este repo es de la clase del viernes");
+console.log ("se me ceerrò y se volviò a abrir")
